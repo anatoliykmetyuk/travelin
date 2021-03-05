95726fc31386b5c377ba58f6e2a971085d401519
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-03-06 00:15:42 +0100

    Import not visible
    [error] -- [E006] Not Found Error: /Users/kmetiuk/Projects/travelin/game/src/Main.scala:12:37
    [error] 12 |    CzechRepublic extends Country(3, ArrogantBackpacker)
    [error]    |                                     ^^^^^^^^^^^^^^^^^^
    [error]    |                                     Not found: ArrogantBackpacke
    [error] one error found
    [error] one error found
    1 targets failed

--- game/src/Main.scala
+++ game/src/Main.scala
@@ -1,1 +1,19 @@
-@main def Main = println("Hello World :)")
+package game
+
+enum Action {
+  case
+    Bus, Train, Airplane,
+    ArrogantBackpacker
+}
+
+enum Country(value: Int, action: Action | Special) {
+  import Action.ArrogantBackpacker
+  case
+    CzechRepublic extends Country(3, ArrogantBackpacker)
+}
+
+enum Special {
+  case DiplomaticImmunity
+}
+
+@main def Main = println(Country.CzechRepublic)
