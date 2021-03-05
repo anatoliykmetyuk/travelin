75112ec5830fe249bd656612098f744ddf937d13
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-03-06 00:15:42 +0100

    Basic scaffolding

--- game/src/Main.scala
+++ game/src/Main.scala
@@ -1,1 +1,21 @@
-@main def Main = println("Hello World :)")
+package game
+
+enum Action {
+  case
+    Bus, Train, Airplane,
+    ArrogantBackpacker,
+    Imperialism,
+}
+
+enum Country(value: Int, action: Action | Special) {
+  case
+    CzechRepublic extends Country(3, Action.ArrogantBackpacker)
+    Belgium(6, Imperialism)
+
+}
+
+enum Special {
+  case DiplomaticImmunity
+}
+
+@main def Main = println(Country.CzechRepublic)
