fe9642bcb501d245eb16cd1f9d80c6fe3ddddef8
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-03-06 11:33:17 +0100

    Test for correct game setup added

--- build.sc
+++ build.sc
@@ -1,13 +1,10 @@
 import mill._, scalalib._
 
-trait Scala3Module extends ScalaModule {
+object game extends ScalaModule {
   def scalaVersion = "3.0.0-RC1"
-}
-
-object game extends Scala3Module {
   def publishVersion = "0.1.0"
 
-  object test extends Scala3Module with TestModule {
+  object test extends ScalaModule with TestModule with Tests {
     def testFrameworks = Seq("utest.runner.Framework")
     def ivyDeps = Agg(ivy"com.lihaoyi::utest::0.7.7")
   }
--- game/test/src/GameTests.scala
+++ game/test/src/GameTests.scala
@@ -4,7 +4,16 @@ import utest._
 
 object GameTests extends TestSuite:
   val tests = Tests {
-    test("Hello World") {
-      assert(false)
+    test("Setup deals 8 cards to each player") {
+      val game = new Game
+      assert(game.player1.hand.isEmpty)
+      assert(game.player2.hand.isEmpty)
+      assert(game.commonCards == null)
+      assert(game.discard.isEmpty)
+
+      game.setup()
+      assert(game.player1.hand.size == 8)
+      assert(game.player2.hand.size == 8)
+      assertMatch(game.commonCards) { case (a, b, c) =>}
     }
   }
